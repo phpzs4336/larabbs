ceaeb33f86f108413b1b9df46a1f85d90b9217fc
zhangshuai <1121323952@qq.com>
2020-10-13 07:37:41 +0000

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,27 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+// 头像假数据
+$avatars = [
+    'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+    'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+];
+
+$factory->define(User::class, function (Faker $faker) use($avatars) {
+    $datetime = $faker->dateTime();
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'avatar'=>$faker->randomElement($avatars),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => Str::random(10),
+        'introduction' => $faker->sentence(),
+        'created_at' => $datetime,
+        'updated_at' => $datetime,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->times(10)->create();
+
+        $user = User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'summer@example.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
