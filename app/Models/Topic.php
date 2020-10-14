9643649a5549f911113013d43ec2132eff2dd083
zhangshuai <1121323952@qq.com>
2020-10-14 07:25:27 +0000

    翻译 url 标识

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,7 +5,7 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = [
-        'title', 'body', 'category_id', 'excerpt', 'slug'
+        'title', 'body', 'category_id', 'excerpt', 'slug',
     ];
 
     public function category()
@@ -38,5 +38,10 @@ class Topic extends Model
     public function scopeRecentReplied($query)
     {
         return $query->orderBy('updated_at','desc');
+    }
+
+    public function link($params=[])
+    {
+        return route('topics.show',array_merge([$this->id,$this->slug],$params));
     }
 }
